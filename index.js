73727b15c5a7154cc4baed11dd207872049d6177
vverheij <v.verheij@gmail.com>
2018-04-07 22:31:29 +0200

    fished homework

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 
 app.use(express.json());
+
+
 
 const genres = [
     {
@@ -24,15 +27,57 @@ app.get('/',(req, res) => {
 
 app.get('/api/genres/', (req, res) => {
     res.send(genres);
-})
+});
 
 app.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(req.params.id));
     if (!genre) return res.status(404).send('Genre with te given id not found');
     
     res.send(genre);
-})
+});
 
+app.post('/api/genres/',(req, res) => {
+    
+    const result = validateGenre(req.body);
+
+    if (result.error) return res.send(result.error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        type: req.body.type
+    }
+
+    genres.push(genre);
+    res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g =>g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('Genre with given id not found');
+    
+    const {error} = validateGenre(req.body);
+    if (error) return res.status(400).send('Bad request');
+    
+    genre.type  = req.body.type;
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g =>g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('Genre with given id not found');
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+    
+    res.send(genre);
+})
+function validateGenre(genre) {
+    const schema = {
+        type: Joi.string().min(3).required()
+    };
+
+    return result = Joi.validate(genre, schema);
+}
 const port = process.env.port || 30001;
 
 app.listen(port,() => console.log(`Server listening on port ${port}.`));
