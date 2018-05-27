c7e02fee21cc856c621e3c947948c529b6cc61fd
Victor Verheij <v.verheij@gmail.com>
2018-05-27 14:49:17 +0200

    added user allread exsits mesaage

--- routes/users.js
+++ routes/users.js
@@ -21,11 +21,14 @@ router.post('/',async (req, res) => {
 
     if (result.error) return res.send(result.error.details[0].message);
 
-    const user = new User({
+    let user = await User.findOne( {email: req.body.email} );
+    if (user) return res.status(400).send('User already registered');
+
+    user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
-    })
+    });
 
     await user.save();
     res.send(user);
