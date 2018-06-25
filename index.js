aa429abc28c82720aedd5ea6ece73a8d5d30d372
Victor Verheij <v.verheij@gmail.com>
2018-06-25 20:08:57 +0200

    so far so good

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 require('express-async-errors');
-
 const winston = require('winston');
-const error = require('./middleware/error');
 const config = require('config');
-const auth = require('./routes/auth');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-//const Joi = require('joi');
 const express = require('express');
-//const logger = require('./logger');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
 const app = express();  
 
-
+require('./startup/routes')(app);
+require('./startup/db')();
 winston.add(new winston.transports.File({filename: 'logfile.log'})); // 
 
 // for catching everything (error) that is not in a route.
@@ -27,7 +17,6 @@ process.on('uncaughtException', (ex)=> {
     winston.error(ex.message, ex);
     proces.exit(1); // anything except 0 means failure.
 });
-
 
 //winston.handleExceptions(new winston.transports.File({filename: 'uncaughtExceptions.log'})); // does not work
 //winston.exceptions.handle(); // does not work
@@ -48,22 +37,8 @@ if (!config.get('jwtPrivateKey')){
     console.error('FATAL ERROR: private key is not defined');
     process.exit(1);
 }
-mongoose.connect('mongodb://localhost/vidly')
- .then(()=> console.log('connected to mongodb'))
- .catch((err) => console.error('could not connect to mongodb'));
 
-app.use(express.json());
 
-//app.use(logger); 
-//app.use(authenticator);
-//app.use(express.urlencoded());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 app.get('/',(req, res) => {
     //console.log(`request received on port ${port}`);
