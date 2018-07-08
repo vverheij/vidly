0bab45ce0d03a92b5812557d9ae8afe61c54b1e7
Victor Verheij <v.verheij@gmail.com>
2018-07-08 22:13:57 +0200

    wip

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,9 @@ const config = require('config');
 
 module.exports = function() {
     mongoose.connect(config.get('db'))
-        .then(() => winston.info(config.get('db')));
-        //.then(()=> console.log('connected to mongodb'))
+        //.then(() => winston.info(config.get('db')));
+        //.then( () => console.log('connected to mongodb'))
+        .then( () => (config.get('db').indexOf('localhost') > 0 ) 
+            ? winston.info('connected to mongodb on localhost') : winston.info('connected to mongodb'));
 
 }
